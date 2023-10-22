a204a5e5f504e88b55115d58e3606185cf266858
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-10-22 12:21:16 +0200

    Add project files.

---
+++ Uppgift 4.7/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Skriv ett meddelande:");
+        string[] userInput = Console.ReadLine().Split(" ");
+        Console.WriteLine("Hur många steg till höger vill du att meddelandet ska skrivas?");
+        int steps = Convert.ToInt32(Console.ReadLine());
+
+        foreach (string input in userInput)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(input + "\n");
+        }
+
+    }
+}
